c58ca199c07e8aae0c6a7f96b7ac28a3ea86e287
Jhonata Emanuel <xxxonata@gmail.com>
2019-02-22 18:16:00 -0300

    Implementação da logica

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ let cardHTML = '';
 
 images.forEach(img => {
     cardHTML += `
-        <div class="memoria-card">
+        <div class="memoria-card" data-card="${img}">
             <img class="front-face" src="img/${img}">
             <img class="back-face" src="img/js-badge.svg">
         </div>
@@ -24,9 +24,36 @@ cardBoard.innerHTML = cardHTML + cardHTML;
 /* Fim renderização HTML */
 
 const cards = document.querySelectorAll('.memoria-card');
+let primeiroCard, segundoCard;
+
 
 function flipCard(){
     this.classList.add('flip');
+
+    if (!primeiroCard) {
+        primeiroCard = this;
+
+        return false;
+    }
+
+    segundoCard = this;
+
+    verificarCards();
+}
+
+function verificarCards(){
+    let combineCards = primeiroCard.dataset.card === segundoCard.dataset.card;
+
+    !combineCards ? resetarCards(): true;
+}
+
+function resetarCards() {
+    setTimeout(() => {        
+        primeiroCard.classList.remove('flip');
+        segundoCard.classList.remove('flip');
+    }, 1000);
+
+
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
