73c946d744195e80eb5adcd2bbc703a6c7682747
Jhonata Emanuel <xxxonata@gmail.com>
2019-02-22 17:25:55 -0300

    Fim da Renderição HTML

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-const cardBoard = document.querySelector("#cardBoard");
+const cardBoard = document.querySelector("#cardboard");
 const images =  [
     'angular.svg',
-    'aurilia.sgv',
-    'backbone..svg',
-    'ember.svg ',
-    'js-badge.sgv',
+    'aurelia.svg',
+    'backbone.svg',
+    'ember.svg',
+    'js-badge.svg',
     'react.svg',
     'vue.svg'
 ];
 
-let carHTML = '';
+let cardHTML = '';
 
 images.forEach(img => {
     cardHTML += `
-        <div>
-            <img src="img/${img}">
-            <img src="img/js-badge.svg">
+        <div class="memoria-card">
+            <img class="front-face" src="img/${img}">
+            <img class="back-face" src="img/js-badge.svg">
         </div>
     `
-})
+});
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+/* Fim renderização HTML */
+
+const cards = document.querySelectorAll('.memoria-card');
+
