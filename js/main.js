961710aca898816cf14eb2b5b8d563d26441d89f
Jhonata Emanuel <xxxonata@gmail.com>
2019-02-22 17:48:33 -0300

    Fim da implementação do front-end

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ const images =  [
     'aurelia.svg',
     'backbone.svg',
     'ember.svg',
-    'js-badge.svg',
     'react.svg',
     'vue.svg'
 ];
@@ -17,7 +16,7 @@ images.forEach(img => {
             <img class="front-face" src="img/${img}">
             <img class="back-face" src="img/js-badge.svg">
         </div>
-    `
+    `;
 });
 
 cardBoard.innerHTML = cardHTML + cardHTML;
@@ -26,3 +25,8 @@ cardBoard.innerHTML = cardHTML + cardHTML;
 
 const cards = document.querySelectorAll('.memoria-card');
 
+function flipCard(){
+    this.classList.add('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
